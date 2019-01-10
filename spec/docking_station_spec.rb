548f4d1fe517f8788c63799e8aeede18f1bc5916
Vaith Schmitz <vaithschmitz@Vaiths-MacBook-Pro.local>
2019-01-10 16:52:07 +0000

    step 13 fixed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,21 +7,30 @@ end
 
 # check docking methods
 describe DockingStation do
- it {expect(subject).to respond_to(:dock).with(1).argument}
- it {expect(subject).to respond_to(:bike)}
- it "docks a bike" do
-   bike = Bike.new
-   expect(subject.dock(bike)).to eq bike
-   expect{subject.dock(bike)}.to raise_error "Dock full"
-
+  it {expect(subject).to respond_to(:dock).with(1).argument}
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
   end
- 
+  
+
   describe '#release_bike' do
     it "raises an error if no bikes in dock" do
   	expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
+
+  describe "#dock" do
+  	it "raises and error if dock full" do
+  	#dock bike for test
+  	subject.dock(Bike.new)
+  	# raise error because there is a bike docked 
+    expect{subject.dock(Bike.new)}.to raise_error "Dock full"
+    end
   end
- 
 end
 
+ 
 
+
+  
